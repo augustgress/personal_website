7b270f00b144e728ee29dbefbaf47d8fe6525842
August Gress <gress.august@gmail.com>
2020-08-18 22:47:15 -0500

    more testing login script changes

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -12,6 +12,7 @@ describe('LoginComponent', () => {
       imports: [
         ReactiveFormsModule,
         FormsModule,
+        RouterTestingModule,
     ],
       declarations: [ LoginComponent ]
     })
